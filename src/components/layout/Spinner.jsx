93af5679b18d10fd5649faaa07de8831106a0136
avinashvagh <avinashvagh0001@protonmail.com>
2022-08-30 23:06:21 +0530

    loading spinner component added

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,13 @@
+
+import spinner from "./assets/spinner.gif"
+
+function Spinner() {
+  return (
+    <div className="w-100 mt-20">
+<img className="text-center mx-auto" src={spinner} alt="loading..." />
+
+    </div>
+  )
+}
+
+export default Spinner

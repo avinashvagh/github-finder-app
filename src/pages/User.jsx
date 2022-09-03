b9a427c5f1d4ac152e53b914df69a2e2d18f4b65
avinashvagh <avinashvagh0001@protonmail.com>
2022-09-03 06:29:30 +0530

    Move GetUser to Actions file

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,16 +5,23 @@ import Spinner from "../components/layout/Spinner";
 import RepoList from "../components/repos/RepoList";
 import GithubContext from "../context/github/GithubContext";
 import { useParams } from "react-router-dom";
+import { getUser, getUserRepos } from "../context/github/GithubActions";
 
 function User() {
-  const { getUser, user, loading, getUserRepos, repos } =
-    useContext(GithubContext);
+  const { user, loading, repos, dispatch } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
-    getUser(params.login);
-    getUserRepos(params.login);
+    dispatch({ type: "SET_LOADING" });
+    const getUserData = async () => {
+      const userData = await getUser(params.login);
+      dispatch({ type: "GET_USER", payload: userData });
+
+      const userRepoData = await getUserRepos(params.login);
+      dispatch({ type: "GET_REPOS", payload: userRepoData });
+    };
+    getUserData();
   }, []);
 
   const {

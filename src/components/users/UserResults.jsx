115d7f8bc96533106e8a3e75ed6e956b1a34a84e
Raj Thombare <rajthombare3524@gmail.com>
2023-01-16 04:48:49 +0530

    fix typo in grid responsiveness for sm devices

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -8,7 +8,7 @@ function UserResults() {
 
   if (!loading) {
     return (
-      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 ms:grid-cols-2'>
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2'>
         {users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
